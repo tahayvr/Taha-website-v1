9106fd37382abd342a941662141eb51073779c09
Taha Nejad <paprikataha@gmail.com>
2024-02-24 23:41:26 -0800

    added feature
    gear icon toggles the main profile box

---
+++ scripts.js
@@ -0,0 +1,12 @@
+var settingsDiv = document.getElementById("settings");
+
+settingsDiv.addEventListener("click", function () {
+  var profileBoxDiv = document.getElementsByClassName("profile-box-flex")[0];
+
+  // Toggle visibility of main-section
+  if (profileBoxDiv.style.display === "none") {
+    profileBoxDiv.style.display = "flex";
+  } else {
+    profileBoxDiv.style.display = "none";
+  }
+});
